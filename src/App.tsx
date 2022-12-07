79e10b4b6c309b63b807b43006e1b503c1e79c92
Kwon Valerii <wellko24@gmail.com>
2022-12-07 18:33:57 +0600

    added Routes in App.tsx and BrowserRouter in index.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import {Route, Routes} from "react-router-dom";
+import NavBar from "./Components/NavBar/NavBar";
+import AdminPage from "./Containers/AdminPage/AdminPage";
+import HomePage from "./Containers/HomePage/HomePage";
+import ContentPage from "./Containers/ContentPage/ContentPage";
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
+      <Routes>
+        <Route path='/' element={(<HomePage/>)}/>
+        <Route path='/pages/:pageName' element={(<ContentPage/>)}/>
+        <Route path='/pages/admin' element={<AdminPage/>}/>
+      </Routes>
     </div>
   );
 }
